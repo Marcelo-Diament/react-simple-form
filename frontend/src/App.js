b357bcf60e380e568b8e7e517c2ed7d92b12107e
Marcelo-Diament <marcelodiament@gmail.com>
2020-11-28 10:44:41 -0300

    feature: pure HTML form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
+      <main>
+        <form action="" method="get">
+          <label htmlFor="name">Nome Completo</label>
+          <input type="text" name="name" key="name"/>
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" key="email"/>
+          <button>Enviar</button>
+        </form>
+      </main>
     </div>
   );
 }
