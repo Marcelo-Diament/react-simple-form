ad54959b990c3076e14b5c90390a21ca3ced595f
Marcelo-Diament <marcelodiament@gmail.com>
2020-11-28 12:23:42 -0300

    feature: FormController component

---
+++ frontend/src/components/FormController/index.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import Form from '../Form'
+import Input from '../Input'
+
+const FormController = ({ ...props }) => {
+  const { action, method, inputs } = props  
+
+  return (
+    <>
+      <Form
+        action={action}
+        method={method}
+      >
+        {inputs.map((input) => {
+          const { name, type, label, required } = input
+          return (
+            <Input
+              name={name}
+              type={type}
+              label={label}
+              key={name}
+              required={required}
+            />
+          )
+        })}
+        <button>Enviar</button>
+      </Form>
+    </>
+  )
+}
+
+export default FormController
